30c560abe3473fad4a5ae5adb9f8a042c28c1d26
Martin Henriksen <martin.henriksen.92@gmail.com>
2018-03-30 23:43:17 +0200

    add constructor for google drive

--- src/Google/GoogleDrive.php
+++ src/Google/GoogleDrive.php
@@ -2,7 +2,16 @@
 
 namespace Cruise\Google\GoogleDrive;
 
+use Google_Client;
+
 class GoogleDrive
 {
+    protected $client;
 
+    public function __construct($appName, $appKey)
+    {
+        $client = new Google_Client();
+        $client->setApplicationName($appName);
+        $client->setDeveloperKey($appKey);
+    }
 }
