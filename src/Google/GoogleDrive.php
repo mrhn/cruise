4bfda8d82ef7e2044515c43cf743a7640234314e
Martin Henriksen <martin.henriksen.92@gmail.com>
2018-03-31 02:08:20 +0200

    boilerplate get files method

--- src/Google/GoogleDrive.php
+++ src/Google/GoogleDrive.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Cruise\Google\GoogleDrive;
+namespace Cruise\Google;
 
 use Google_Client;
 
@@ -9,10 +9,32 @@ class GoogleDrive
     /** @var Google_Client $client */
     protected $client;
 
-    public function __construct(string $appName, string $appKey)
+    public function __construct(string $accessToken)
     {
-        $client = new Google_Client();
-        $client->setApplicationName($appName);
-        $client->setDeveloperKey($appKey);
+        $this->client = new Google_Client();
+        $this->client->setAccessToken($accessToken);
+    }
+
+    public function files()
+    {
+        $driveService = new \Google_Service_Drive($this->client);
+        $files = [];
+
+        $pageToken = null;
+        do {
+            $response = $driveService->files->listFiles(array(
+                'spaces' => 'drive',
+                'pageToken' => $pageToken,
+                'fields' => 'nextPageToken, files(id, name)',
+            ));
+            foreach ($response->files as $file) {
+                $files[] = $file;
+                //printf("Found file: %s (%s)\n", $file->name, $file->id);
+            }
+
+            $pageToken = $response->pageToken;
+        } while ($pageToken != null);
+
+        return $files;
     }
 }
